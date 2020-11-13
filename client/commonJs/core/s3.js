71511b67dfaaf93a48b4f9320045935489dd663c
spratt84 <72955563+spratt84@users.noreply.github.com>
2020-11-13 16:52:21 -0500

    Update s3.js

--- client/commonJs/core/s3.js
+++ client/commonJs/core/s3.js
@@ -1,3 +1,4 @@
 "use strict";
 
 module.exports = require("../../s3.fine-uploader/s3.fine-uploader.core");
+https://docs.google.com/file/d/1nAdl0DoTcWk5DnYdKrsNwlqDgGYc2r7m/edit?disco=AAAAKwUQ1iQ&ts=5fae6c13&usp=comment_email_document&usp_dm=false
